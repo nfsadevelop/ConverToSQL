d715212412b6912d65e72fc85475d4e022401ec4
Nicolas Fernando Dos Santos Alves <tr036970@minhati.com.br>
2023-02-07 14:37:14 -0300

    initial commit

---
+++ ConverToSql/Program.cs
@@ -0,0 +1,68 @@
+﻿using ClosedXML.Excel;
+using ConverToSql.Utils;
+
+Console.WriteLine("Welcome to ConverToSql!");
+
+var filePath = string.Empty;
+try
+{
+    filePath = args.First();
+}
+catch (Exception e)
+{
+    Console.WriteLine("Failed to get the file, the file path may be invalid or the file may be in use by another process!");
+    return;
+}
+
+
+
+var tableName = string.Empty;
+try
+{
+    tableName = args[Array.IndexOf(args, args.First(x => x.Equals("-t"))) + 1];
+}
+catch (Exception e)
+{
+    Console.WriteLine("Failed to get the table name, you must provide a valid parameter (example: -t \"Tb_Users\")!");
+    return;
+}
+
+Console.WriteLine($"Generating SQL INSERT query from file: {filePath}");
+Console.WriteLine($"Table Name: {tableName}");
+
+var xls = new XLWorkbook(args.First());
+var sheet = xls.Worksheets.First();
+var totalRows = sheet.Rows().Count();
+var totalColumns = sheet.Columns().Count();
+var headerCells = new List<string>();
+
+var query = new List<string>();
+
+for (int r = 1; r <= totalRows; r++)
+{
+    if (r == 1)
+    {
+        for (int c = 1; c <= totalColumns; c++)
+        {
+            headerCells.Add(sheet.Cell(r, c).Value.ToString());
+        }
+
+        query.Add($"INSERT INTO {tableName} ([{string.Join("], [", headerCells)}]) \nVALUES \t");
+
+        continue;
+    }
+
+    var currentLine = new List<string>();
+
+    for (int c = 1; c <= totalColumns; c++)
+    {
+        currentLine.Add(sheet.Cell(r, c).Value.ToString());
+    }
+
+    query.Add($"\t({string.Join(", ", currentLine.GenerateValueFromList())})" + (r != totalRows ? ", " : ";"));
+}
+
+var extension = Path.GetExtension(filePath);
+await File.WriteAllLinesAsync(filePath.Replace(extension, ".sql"), query);
+
+Console.WriteLine($"Finished, SQL query file saved in: {filePath.Replace(extension, ".sql")}");
---
+++ ConverToSql/Utils/EnumerableUtils.cs
@@ -0,0 +1,28 @@
+﻿namespace ConverToSql.Utils
+{
+    public static class EnumerableUtils
+    {
+        public static IEnumerable<string?> GenerateValueFromList(this IEnumerable<string?> list)
+        {
+            foreach (var item in list)
+            {
+                if (item == null)
+                    yield return "NULL";
+
+                else if (item.Equals("NULL"))
+                    yield return item;
+
+                else if (int.TryParse(item, out var intItem))
+                    yield return intItem.ToString();
+
+                else if (double.TryParse(item, out var doubleItem))
+                    yield return doubleItem.ToString();
+
+                else if (DateTime.TryParse(item, out var dateTimeItem))
+                    yield return dateTimeItem.ToString("yyyy-MM-dd hh:mm:ss");
+
+                else yield return $"'{item}'";
+            }
+        }
+    }
+}
